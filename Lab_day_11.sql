e8a20de35ed6a1271e437df4dd6423d4ff213f97
Annbeele <annabeele7@gmail.com>
2021-06-14 15:08:05 +0200

    uploading files

---
+++ Lab_day_11.sql
@@ -0,0 +1,21 @@
+# Review the tables in the database.
+#     Done --> y right clicked on every table and cliked on "select rows" to open each table and study it
+# Explore tables by selecting all columns from each table or using the in built review features for your client.
+#      Same as above
+#3. Select one column from a table. Get film titles.
+SELECT title FROM film;
+
+#4. Select one column from a table and alias it. Get unique list of film languages under the alias language. Note that we are not asking you to obtain the language per each film, but this is a good time to think about how you might get that information in the future.
+select distinct name  as language from language;
+
+# 5.
+# 5.1 Find out how many stores does the company have?
+select count(*) from store;
+
+# 5.2 Find out how many employees staff does the company have?
+select count(*) from staff;
+
+# 5.3 Return a list of employee first names only?
+select first_name from staff;
+
+
